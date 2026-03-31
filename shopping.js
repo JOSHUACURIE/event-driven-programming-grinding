c6b765b0bc039d848d6bffd4fd16979a2a6d9c93
JOSHUACURIE <joshuajuma039@gmail.com>
2026-03-31 14:45:56 +0300

    first implementation

---
+++ shopping.js
@@ -0,0 +1,19 @@
+const { error } = require('console');
+const EventEmitter=require('events');
+const myEmitter=new EventEmitter();
+let orderId=0;
+
+myEmitter.on("order-placed",(orderId)=>{
+    console.log(`order id ${orderId} place confirming and sending email`);
+})
+myEmitter.on("error",(err,id)=>{
+    console.log(`System error on order ${id} error: ${err.message}`)
+})
+setInterval(()=>{
+    console.log("Use clicked 'Buy'... ");
+ if(Math.random()< 0.2){
+    myEmitter.emit("error",new Error("Payment GateWay Down"),++orderId);
+ }else{
+    myEmitter.emit("order-placed",++orderId);
+ }
+},1000)
